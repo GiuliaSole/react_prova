6a2f0190ee1883afd49299ab7197487a7e587f15
GiuliaSole <aldegherigiuliasole@gmail.com>
2025-02-04 11:18:40 +0100

    Java
    Messo java

---
+++ index.js
@@ -0,0 +1,36 @@
+// comando map
+/*
+let numeri = [1,2,3];
+let doppi = numeri.map ((numero) => {
+    return numero * 2
+});
+console.log(doppi);
+*/
+//comando filter
+/*
+let numeri2 = [1,2,3,4,5];
+let pari = numeri2.filter ((numero) => {
+    return numero % 2 === 0
+});
+console.log(pari)
+*/
+// combando spread
+/*
+let numero = [1,2,3];
+let nuovo = [...numero,4,5];
+console.log(numero);
+console.log(nuovo);
+*/
+/*
+const persona = {
+    nome: "Gino",
+    cognome: "Rossi"
+}
+console.log (persona);
+
+const nuovaPersona = {
+    ...persona,
+    eta: 30
+};
+console.log(nuovaPersona);
+*/
